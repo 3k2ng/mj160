3c07ed7a594da584321d0e9a1576ad817b472047
PastThePixels <pastthescreen@protonmail.com>
2024-06-08 22:07:29 -0600

    Change player size and physics
    Another thing we need to edit after we have the scale for everything
    mapped out.

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,9 +3,10 @@ extends CharacterBody2D
 
 @onready var sprite = $AnimatedSprite2D
 
+@export var gravity_scale : float = 1
 
-const SPEED = 600.0
-const JUMP_VELOCITY = -1000.0
+@export var SPEED = 600.0
+@export var JUMP_VELOCITY = -500.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -19,7 +20,7 @@ func _physics_process(delta):
 func handle_inputs(delta):
 	# Add the gravity.
 	if not is_on_floor():
-		velocity.y += gravity * delta
+		velocity.y += gravity * delta * gravity_scale
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
