3e1e1aea3971f670092af25b3aed6c45137547e8
krill <krill@krill-pc.net>
2024-06-09 00:36:04 -0600

    fixed steering
    it's smoother now but still have to add timer mechanic so it gets faster as more time has passed

--- scenes/shadow_moster.gd
+++ scenes/shadow_moster.gd
@@ -1,10 +1,8 @@
 extends CharacterBody2D
 
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-const R = 100.0
+const R = 130.0
+const k = 10.0
+const epsilon = 10.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -12,15 +10,12 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var stage = 0
 var acceleration = Vector2(0,0)
 
-var k = 10.0
 var x_0
-var prev_C = Vector2(0,0)
-var C_static
-
-var epsilon = 10.0
-
+var C_static = Vector2(0,0)
 var timer = 0.0
-var last_launch = 0.0
+var last_launch = -15.0
+
+
 func exponent(b, x):
 	var result = 1
 	while x > 0:
@@ -40,17 +35,13 @@ func normalize(vec):
 func _physics_process(delta):
 	
 	var C = get_viewport().get_mouse_position()
-	
-	
-	#C = Vector2(100,150)
-	#position = Vector2(0,0)
 		
-	print(stage)
+	#print(stage)
 	#print("v:")
 	#print(velocity)
 	#print("a:")
 	#print(acceleration)
-	print("r:")
+	#print("r:")
 	if(stage < 2):
 		print(length(position - C))
 	else:
@@ -59,45 +50,63 @@ func _physics_process(delta):
 	# finding the circle
 	if stage == 0:
 		
-		var u = C - position
+		var u = position - C
 		var d = sqrt(dot(u,u) + R*R)
-		var alpha = atan2(u.y,u.x)
+		var alpha = atan2(-u.y,-u.x)
 		var beta = atan2(R,length(u))
 		var theta = alpha - beta
-		if(C != prev_C):
-			x_0 = Vector2(position.x + d * cos(theta), position.y + d * sin(theta))
 	
-		acceleration = (2*(C - position))/(5*5)
+		acceleration = -2*(position - C - velocity * 8.0)/(8.0*8.0)
 		#steering
-		if length(position - C) < 4*R and length(position - C) > R + epsilon and dot(velocity, u) > 0:
-			var n = normalize(C - position)
+		if length(u) < 3.5*R and length(u) > R + epsilon and dot(velocity, u) < 0:
+			if(length(u) > 2*R):
+				velocity += acceleration * delta
+			
 			var unit_v = normalize(velocity)
-			var unit_a_c = normalize(n - dot(n,unit_v) * unit_v)
+			var n = normalize(position - C)
 			
-			var r = (dot(u,u) - exponent(R + epsilon, 2))/(2*(R + epsilon - dot(u,unit_a_c)))
-			acceleration = dot(velocity, velocity)/r * unit_a_c
-			acceleration = acceleration - dot(acceleration, unit_v)*unit_v
+			var b = dot(2*unit_v, n)
+			var c = dot(n,n) - exponent(R, 2)
 			
-		elif length(position - C) > 5 * R && dot(u,velocity) < 0:
-			acceleration += -(velocity + acceleration * delta)/5
+			if b*b - 4*c > 0:
+				acceleration = Vector2(0,0)
+				var unit_a_c = normalize(n - dot(n,unit_v) * unit_v)
+				unit_a_c = normalize(unit_a_c - dot(unit_a_c, unit_v)*unit_v)
+				
+				var r = (dot(u,u) - exponent(R + epsilon, 2))/(2.0*(R + epsilon - dot(u,unit_a_c)))
+				acceleration += dot(velocity, velocity)/r * unit_a_c
+				#acceleration = length(acceleration) * normalize(unit_a_c - dot(unit_v, unit_a_c) * unit_v)
+
 			
-		elif length(position - C) < R + 1.1* epsilon:
+			else:
+				if(length(u) > 2*R):
+					velocity -= acceleration * delta
+			
+			
+		elif length(position - C) > 7 * R && dot(u,velocity) > 0:
+			acceleration -= 5.0 * (velocity + acceleration * delta)
+			
+		elif length(position - C) <= R + 2*epsilon:
 			stage = 1
-			u = x_0 - C
-			x_0 = C + normalize(u) * R
-			velocity = velocity - dot(normalize(u), velocity)*normalize(u)
+			position = C + normalize(position - C) * R
+			velocity = velocity - length(velocity)*dot(normalize(-u), normalize(velocity))*normalize(-u)
+			acceleration = length(velocity)*length(velocity)/R * normalize(-u)
 		
 	
 	elif(stage == 1):
-		position = C + normalize(position - C) * R
-		#acceleration = dot(velocity, velocity)/R * normalize(position - C)
-		if timer - last_launch > 5.0:
+		var N = normalize(position - C)
+		var T = normalize(velocity)
+		position = C + N * R
+		acceleration = -4*N
+		velocity = velocity - length(velocity)*dot(N, normalize(velocity))*N
+		if timer - last_launch > 10.0:
 			stage = 2
-			C_static = C
 			x_0 = position
 			velocity = Vector2(0,0)
 		
 	elif stage == 2 or stage == 3:
+		if stage == 2:
+			C_static = C
 		var T = normalize(Vector2((x_0 - C_static).y, (C_static - x_0).x))
 		var N = normalize(x_0 - C_static)
 		var a = C_static + R * T
@@ -111,7 +120,7 @@ func _physics_process(delta):
 		
 		if(stage == 2):
 			acceleration = Vector2(0,0)
-			velocity = N * 1.0 *(2* R + epsilon - length(position - C_static))
+			velocity = N * 1.0 *(2 * R + epsilon - length(position - C_static))
 			
 			if length(position - C_static) > 2*R:
 				stage = 3
@@ -119,28 +128,22 @@ func _physics_process(delta):
 			acceleration = F_sa + F_sb
 			acceleration = dot(acceleration, -N) * -N
 			
-			print(acceleration)
-			print(velocity)
 			if dot(-N, acceleration) <= 0 and length(position - C_static) < R:
 				acceleration = Vector2(0,0)
 				stage = 4
 				last_launch = timer
 				
 	elif(stage == 4):
-		print(timer)
-		print(last_launch)
-		if length(position - C) > 5 * R && dot(position - C,velocity) > 0:
-			acceleration = -(velocity + acceleration * delta)
+		if length(position - C_static) > 5 * R && dot(position - C_static,velocity) > 0:
+			acceleration = -5.0*(velocity + acceleration * delta)
 	
-		if timer - last_launch > 1.0:
-		
+		if timer - last_launch > 1.7:
 			stage = 0
 			
 	
 	velocity += acceleration * delta
 	position += velocity * delta
 	timer += delta
-	prev_C = C
 	
-	print("\n")
+	#print("\n")
 	move_and_slide()
