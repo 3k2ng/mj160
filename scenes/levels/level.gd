8ddc52f402beac4f466e562dc89f2be987669744
PastThePixels <pastthescreen@protonmail.com>
2024-06-09 12:35:25 -0600

    Clean up level.gd, add light switch

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -6,22 +6,24 @@ signal completed
 
 var spawn_position : Vector2
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+var switched = false
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
+var exit_door : AnimatedSprite2D
 
 func _on_tile_map_child_entered_tree(node):
 	if node.is_in_group("enter_door"):
 		spawn_position = node.global_position
 		emit_signal("initialised", spawn_position)
 	if node.is_in_group("exit_door"):
+		exit_door = node
 		node.connect("exited", signal_completed)
+	if node.is_in_group("light_switch"):
+		node.connect("switched", switch_light)
+
+# TODO Make everything go dark
+func switch_light():
+	switched = true
+	exit_door.disabled = false
 
 func signal_completed():
 	emit_signal("completed")
