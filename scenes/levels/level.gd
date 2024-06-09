d3a46772a2a810b25c03b6579dd9b8ab2a18d26c
PastThePixels <pastthescreen@protonmail.com>
2024-06-09 12:19:37 -0600

    Update spawn_position dyanmically
    depending on wherever the position of the enter door is in the TileMap

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
-@onready var spawn_coords = $Player.position
+signal initialised
+
+var spawn_position : Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,3 +12,9 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_tile_map_child_entered_tree(node):
+	if node.is_in_group("enter_door"):
+		spawn_position = node.global_position
+		emit_signal("initialised", spawn_position)
