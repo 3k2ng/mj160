9a956d0a637a9c38da0316390523443c1983ac60
PastThePixels <pastthescreen@protonmail.com>
2024-06-09 12:27:25 -0600

    exit door functionality

--- objects/tiles/exit_door.gd
+++ objects/tiles/exit_door.gd
@@ -1,6 +1,8 @@
 extends AnimatedSprite2D
 
+signal exited
 
 func _on_area_2d_body_entered(body):
 	if body.is_in_group("player"):
 		play("close")
+		emit_signal("exited")
--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 signal initialised
 
+signal completed
+
 var spawn_position : Vector2
 
 # Called when the node enters the scene tree for the first time.
@@ -18,3 +20,8 @@ func _on_tile_map_child_entered_tree(node):
 	if node.is_in_group("enter_door"):
 		spawn_position = node.global_position
 		emit_signal("initialised", spawn_position)
+	if node.is_in_group("exit_door"):
+		node.connect("exited", signal_completed)
+
+func signal_completed():
+	emit_signal("completed")
