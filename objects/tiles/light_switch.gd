f22958f613209b9d58093297ec64dac2a7008a72
PastThePixels <pastthescreen@protonmail.com>
2024-06-09 21:25:31 -0600

    add timer to light switch

--- objects/tiles/light_switch.gd
+++ objects/tiles/light_switch.gd
@@ -5,10 +5,11 @@ signal switched
 var on = true
 
 func _on_area_2d_body_entered(body):
-	if body.is_in_group("player"):
+	if body.is_in_group("player") and $Timer.time_left == 0:
 		if on:
 			play("switch")
 		else:
 			play_backwards("switch")
 		emit_signal("switched")
 		on = not on
+		$Timer.start()
