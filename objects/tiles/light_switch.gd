40461cf85bc46c261040e3b15dd00912bd48a837
jukmifgguggh1337 <spamandjunkmail9@gmail.com>
2024-06-09 20:02:53 -0600

    light switch sound effect

--- objects/tiles/light_switch.gd
+++ objects/tiles/light_switch.gd
@@ -1,8 +1,11 @@
 extends AnimatedSprite2D
 
+@onready var switch_sfx = $SwitchSFX
+
 signal switched
 
 func _on_area_2d_body_entered(body):
 	if body.is_in_group("player"):
 		play("switch")
 		emit_signal("switched")
+		switch_sfx.play()
