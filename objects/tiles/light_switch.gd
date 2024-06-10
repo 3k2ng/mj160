2356505e8d3bd60c03577991a164c62c22f4a62c
PastThePixels <pastthescreen@protonmail.com>
2024-06-09 20:43:08 -0600

    Light switch texture

--- objects/tiles/light_switch.gd
+++ objects/tiles/light_switch.gd
@@ -2,7 +2,13 @@ extends AnimatedSprite2D
 
 signal switched
 
+var on = false
+
 func _on_area_2d_body_entered(body):
 	if body.is_in_group("player"):
-		play("switch")
+		if on:
+			play("switch")
+		else:
+			play_backwards("switch")
 		emit_signal("switched")
+		on = not on
