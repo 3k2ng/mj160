6e9cdeb68f2978e4788b192a7b0f48a3845c6a64
krill <krill@krill-pc.net>
2024-06-09 19:58:52 -0600

    monster visibility - minor changes

--- scenes/monster_sprite.gd
+++ scenes/monster_sprite.gd
@@ -1,6 +1,5 @@
 extends AnimatedSprite2D
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -23,3 +22,4 @@ func _process(delta):
 				animation = "down"
 	else:
 		animation = "default"
+
